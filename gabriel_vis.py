834801bf0a974416ff0c679c5d04ba750e8d0d6a
Gabriel Abad <pedroabadg961@gmail.com>
2023-06-26 12:10:37 -0300

    Primeiro gráfico quase completo

--- gabriel_vis.py
+++ gabriel_vis.py
@@ -1,8 +1,74 @@
-#Importei do meu módulo a função 'ocorrencia_por_pais' e o csv 'df_cafe'
-from manipulacao_dados_gabriel import ocorrencia_por_pais, df_cafe
+#Import para a visualização em bokeh
+from bokeh.plotting import figure
+from bokeh.models import Range1d,HoverTool
+from bokeh.io import output_file, save, show
+import numpy as np
 
-#Guardei em uma variável as quantidades de lotes produzidos por país
-lotes_produzidos = ocorrencia_por_pais(df_cafe).data['valores']
+#Importei do meu módulo a função 'ocorrencia_por_pais', o csv 'df_cafe', a função 'peso_total_por_pais' e a 'media_por_pais'
+from manipulacao_dados_gabriel import ocorrencia_por_pais, df_cafe, peso_total_por_pais, media_por_pais, correlacao_quantidade_qualidade
 
-#Guardei em uma variável o nome dos países correspondentes 
-nome_dos_paises = ocorrencia_por_pais(df_cafe).data['indices']
+
+
+output_file('gabriel_vis.html')
+
+# Dados
+x = correlacao_quantidade_qualidade.data['media']
+y = correlacao_quantidade_qualidade.data['producao']
+
+# Calcular os coeficientes da reta de regressão
+coefficients = np.polyfit(x, y, 1)
+slope = coefficients[0]
+intercept = coefficients[1]
+
+# Criar um objeto Figure
+figure = figure()
+
+# Plotar os pontos de dispersão
+figure.circle(source=correlacao_quantidade_qualidade, x='media', y='producao', fill_color="SaddleBrown", line_color="SaddleBrown", size=10)
+
+# Traçar a reta de regressão
+x_regression = np.array([min(x), max(x)])
+y_regression = slope * x_regression + intercept
+figure.line(x_regression, y_regression, line_color='#3D2923', line_width=3)
+
+# Configurações adicionais do gráfico
+figure.width = 1280
+figure.height = 720
+figure.background_fill_color = "GoldenRod"
+figure.title.text = 'Quantidade é Qualidade?'
+figure.title.text_color = "SaddleBrown"
+figure.title.text_font = "Arial"
+figure.title.text_font_size = "42px"
+figure.title.align = "center"
+figure.x_range.start = 7
+figure.x_range.end = 8
+figure.y_range.start = -200
+figure.y_range.end = 350000
+figure.xaxis[0].ticker.desired_num_ticks = 2
+figure.yaxis[0].ticker.desired_num_ticks = 3
+figure.grid.grid_line_alpha = 0
+figure.xaxis.axis_label = "Overall"
+figure.yaxis.axis_label = "Produção (kg)"
+figure.xaxis.axis_label_text_font_size = "16pt"
+figure.yaxis.axis_label_text_font_size = "16pt"
+figure.xaxis.axis_label_text_color = "#3D2923"
+figure.yaxis.axis_label_text_color = "#3D2923"
+figure.xaxis.minor_tick_line_color = "#3D2923"
+figure.yaxis.minor_tick_line_color = "#3D2923"
+figure.xaxis.minor_tick_in = 2
+figure.yaxis.minor_tick_in = 2
+figure.axis.major_label_text_color = "#3D2923"
+#Adicionei interação para ao passar o mouse por cime da bolinha aparecer informações sobre o país, a produção e a qualidade do café
+tooltips = [("País", "@paises"),
+            ("Produção em kg", "@producao"),
+            ("Qualidade do Café", "@media")]
+figure.add_tools(HoverTool(tooltips=tooltips))
+
+#Adicionei um retângulo onde ficará minha anotação
+figure.rect(x=7.357, y=200000, width=0.328, height=30000, fill_alpha=0, line_color='#3D2923', line_width=2)
+
+# Adicionar o texto na posição y=110000
+figure.text(x=7.21, y=200000, text=['A reta de regressão está muito inclinada por conta'], text_color='#3D2923', text_font_size='12pt')
+figure.text(x=7.21, y=190000, text=['da Ethiopia (usar o wheel zoom para ver o outlier)'], text_color='#3D2923', text_font_size='12pt')
+# Exibir o gráfico
+show(figure)
--- manipulacao_dados_gabriel.py
+++ manipulacao_dados_gabriel.py
@@ -5,7 +5,7 @@ from bokeh.models import ColumnDataSource
 import pandas as pd
 
 #Botei em uma variável o csv usado no trabalho
-df_cafe = pd.read_csv('df_arabica_clean.csv')
+df_cafe = pd.read_csv('cleaned_coffee_dataset.csv')
 
 """Verifiquei quantos lotes foram produzidos por país, e criei o
 CDS com o nome do país e seu respectivo numero de lotes produzidos"""
@@ -15,3 +15,25 @@ def ocorrencia_por_pais(df_cafe):
     nome_do_pais_correspondente = producao_total.index
     novo_cds = ColumnDataSource(data=dict(valores=numero_de_lotes, indices=nome_do_pais_correspondente))
     return novo_cds
+
+"""Criei uma função para criar um cds com os paises como index e a sua produção total em kg"""
+def peso_total_por_pais(df_cafe):
+    df_cafe['Peso da Mochila'] = df_cafe['Bag Weight'].str.replace(' kg', '').astype(int)
+    df_cafe['Peso Total Por Produtora'] = df_cafe['Number of Bags'] * df_cafe['Peso da Mochila']
+    producao_por_pais_em_kg = df_cafe.groupby('Country of Origin')['Peso Total Por Produtora'].sum().reset_index()
+    producao_por_pais_em_kg = producao_por_pais_em_kg.set_index('Country of Origin')
+    novo_cds = ColumnDataSource(data=dict(producao=producao_por_pais_em_kg['Peso Total Por Produtora'].tolist(), paises=producao_por_pais_em_kg.index.tolist()))
+    return novo_cds
+
+"""Função para criar objetos em cds com a media da qualidade do cafe por pais"""
+def media_por_pais(df_cafe):
+    media_da_qualidade_do_cafe = df_cafe.groupby('Country of Origin')['Overall'].mean()
+    novo_df = pd.DataFrame({'País': media_da_qualidade_do_cafe.index, 'Média das Notas': media_da_qualidade_do_cafe.values})
+    novo_cds = ColumnDataSource(data=dict(paises=novo_df['País'], medias=novo_df['Média das Notas']))
+    return novo_cds
+
+"""Criei um cds com a produção total por país, e a média da qualidade por país, para fazer o gráfico"""
+Producao = peso_total_por_pais(df_cafe).data['producao']
+Paises = media_por_pais(df_cafe).data['paises']
+Media = media_por_pais(df_cafe).data['medias']
+correlacao_quantidade_qualidade = ColumnDataSource(data=dict(media=Media,producao=Producao, paises=Paises))
